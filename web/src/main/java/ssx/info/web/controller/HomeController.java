fb030c3dde7c77b656bc4cdf7e1116ee9187cc19
SSX <mr.dimov77@gmail.com>
2023-10-06 20:12:19 +0300

    Adding new dependencies + changing controller type

--- web/src/main/java/ssx/info/web/controller/HomeController.java
+++ web/src/main/java/ssx/info/web/controller/HomeController.java
@@ -1,16 +1,17 @@
 package ssx.info.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping({"/", "home"})
     public String home(Model model) {
 //        model.addAttribute("users", userService.getAll());
-        return "Home";
+        return "home";
     }
 
     @GetMapping("/profile")
