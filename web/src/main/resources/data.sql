33dc1360a2e52c53ef20e58e0f015f40028e38c0
SSX <mr.dimov77@gmail.com>
2023-10-16 17:50:52 +0300

    Minor changes in data.sql

--- web/src/main/resources/data.sql
+++ web/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO users VALUES (1,'admin@gmail.com','admin','$2a$10$gqHrslMttQWSsDSVRTK1OehkkBiXsJ/a4z2OURU./dizwOQu5Lovu','admin');
 INSERT INTO users VALUES (2,'test@gmail.com','test', '$2a$12$TYSPPDsgR1T9vpgMSavOteZoqzjGVLt7rzsqKLrGL4oQdE3rWDNru','test');
 
-INSERT INTO role VALUES (1,'ROLE_ADMIN');
-INSERT INTO role VALUES (2,'ROLE_USER');
+INSERT INTO roles VALUES (1,'ROLE_ADMIN');
+INSERT INTO roles VALUES (2,'ROLE_USER');
 
 INSERT INTO users_roles VALUES (1,1),(2,2);
